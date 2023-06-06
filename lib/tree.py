65209506e45ea881b98a003161862da9ad418289
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-06-06 14:36:12 -0400

    completed

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,17 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    
+    result = []
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      # 1. Remove the first node from the `nodes_to_visit` list
+      node = nodes_to_visit.pop(0)
+      # 2. Add its value to the result list
+      if node['id'] == id:
+        return node
+      # 3. Add its children (if any) to the BEGINNING of the `nodes_to_visit` list
+      nodes_to_visit = node['children'] + nodes_to_visit
+
+    return None
